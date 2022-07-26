a351a9ca946f0eff117aacd84460efa11f7a9188
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-27 02:42:51 +0530

    Added: Add A New Contact To Address Book

--- src/com/bridgelab/addressbookoops/AddressBookMain.java
+++ src/com/bridgelab/addressbookoops/AddressBookMain.java
@@ -3,6 +3,10 @@ package com.bridgelab.addressbookoops;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book");
+		AddressBookOperations add = new AddressBookOperations();
+
+		add.addContact();
+		System.out.println(add.display());
 
 	}
 
